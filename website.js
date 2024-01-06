ff694466ebbae2d3b4e1248d9e94a914e5714c4f
Prabhjot-Kaur123 <155060916+Prabhjot-Kaur123@users.noreply.github.com>
2024-01-06 16:17:00 +0530

    Add files via upload
    website using html css and js.....

---
+++ website.js
@@ -0,0 +1,12 @@
+var p=document.getElementById("pages1")
+p.addEventListener("mousemove",function(){
+    var pp=document.getElementById("kitediv")
+    pp.style.display="block"
+    
+})
+var o=document.getElementById("im1")
+o.addEventListener("mousemove",function(){
+    var pp=document.getElementById("kitediv")
+    pp.style.display="none"
+    // location.reload()
+})
